f082ff7964ea9e9eabcfb75894d080fe1164d109
Tyflo29 <tyflo29400@gmail.com>
2020-12-06 15:32:40 +0100

    Update JSS.dragon.js

--- JSS.dragon.js
+++ JSS.dragon.js
@@ -1,4 +1,21 @@
 
 goule1.onmouseover = function(){
-	document.getElementById("statsgoule1").style.visibility = "visibilité"
+	document.getElementById("statsgoule1").style.visibility = "visible"
+}
+goule1.onmouseout = function(){
+	document.getElementById("statsgoule1").style.visibility = "hidden"
+}
+
+goule2.onmouseover = function(){
+	document.getElementById("statsgoule2").style.visibility = "visible"
+}
+goule2.onmouseout = function(){
+	document.getElementById("statsgoule2").style.visibility = "hidden"
+}
+
+Boss.onmouseover = function(){
+	document.getElementById("statsboss").style.visibility = "visible"
+}
+Boss.onmouseout = function(){
+	document.getElementById("statsboss").style.visibility = "hidden"
 }
