0dfdbf01d434f42905aaa7c69f8aae4c4f699edd
Tyflo29 <tyflo29400@gmail.com>
2020-12-08 17:43:22 +0100

    choix cible

--- JSS.dragon.js
+++ JSS.dragon.js
@@ -1,20 +1,44 @@
 
-var numerodecible = document.getElementById().innerHTML
 
-ciblegoule1.onclick = function(numerodecible){
-	
-	return
+ciblegoule1.onclick = function(){
+	numerodetarget.innerHTML = 1
 }
 
-
+cibleboss.onclick = function(){
+	numerodetarget.innerHTML = 2
+}
+ciblegoule2.onclick = function(){
+	numerodetarget.innerHTML = 3
+}
 
 attaquePaladin.onclick = function(){
-	degats = 25
-	pvBoss = document.getElementById("pvBoss").innerHTML
-	pvBoss= (pvBoss - degats)
-	document.getElementById("pvBoss").innerHTML = pvBoss
-	if (pvBoss <= 0){
-		document.getElementById("Boss").style.visibility="hidden";
+	degats = 20
+	if (numerodetarget.innerHTML == 2){
+		pvBoss = document.getElementById("pvBoss").innerHTML
+		pvBoss= (pvBoss - degats)
+		document.getElementById("pvBoss").innerHTML = pvBoss
+		if (pvBoss <= 0){
+			document.getElementById("Boss").style.visibility="hidden";
+			document.getElementById("cibleboss").style.visibility="hidden";
+		}
+	}
+	if (numerodetarget.innerHTML == 1){
+		pvGoule1 = document.getElementById("pvGoule1").innerHTML
+		pvGoule1= (pvGoule1 - degats)
+		document.getElementById("pvGoule1").innerHTML = pvGoule1
+		if (pvGoule1 <= 0){
+			document.getElementById("goule1").style.visibility="hidden";
+			document.getElementById("ciblegoule1").style.visibility="hidden";
+		}
+	}
+	if (numerodetarget.innerHTML == 3){
+		pvGoule2 = Number(document.getElementById("pvGoule2").innerHTML)
+		pvGoule2 = (pvGoule2 - degats)
+		document.getElementById("pvGoule2").innerHTML = pvGoule2
+		if (pvGoule1 <= 0){
+			document.getElementById("goule2").style.visibility="hidden";
+			document.getElementById("ciblegoule2").style.visibility="hidden";
+		}
 	}
 }
 
@@ -29,6 +53,7 @@ defensePaladin.onclick = function(){
 
 
 //------------------------InfoBulle apparait--------------------------//
+//--------goule1------------//
 document.getElementById("goule1").onmouseover = function(){
 	document.getElementById("statsgoule1").style.visibility = "visible"
 }
@@ -37,6 +62,7 @@ document.getElementById("goule1").onmouseout = function(){
 	document.getElementById("statsgoule1").style.visibility = "hidden"
 }
 
+//--------boss------------//
 document.getElementById("goule2").onmouseover = function(){
 	document.getElementById("statsgoule2").style.visibility = "visible"
 }
@@ -45,6 +71,7 @@ document.getElementById("goule2").onmouseout = function(){
 	document.getElementById("statsgoule2").style.visibility = "hidden"
 }
 
+//--------goule2------------//
 document.getElementById("Boss").onmouseover = function(){
 	document.getElementById("statsboss").style.visibility = "visible"
 }
